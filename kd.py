ce4a19c89729f25be41cdc2ec2804e82d26ff930
yun <yzhangjy@connect.ust.hk>
2023-11-29 21:07:05 +0800

    Vanilla KD trainer.

---
+++ kd.py
@@ -0,0 +1,102 @@
+# NCCL_P2P_DISABLE=1 deepspeed --include localhost:6,7 kd.py
+
+import os
+# from transformers.utils import logging
+from loguru import logger
+from pathlib import Path
+from transformers import AutoTokenizer
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, GenerationConfig
+from transformers import DataCollatorForSeq2Seq, Seq2SeqTrainer, Seq2SeqTrainingArguments
+from datasets import load_dataset, load_from_disk
+# import deepspeed
+
+from src.trainer import Seq2SeqKDArguments, Seq2SeqKDTrainer
+from src.dataset import samsum
+
+os.environ['CUDA_VISIBLE_DEVICES'] = "6"
+
+# logger = logging.get_logger(__name__)
+
+MODEL_PATH = Path("../models/gpt2/base/")
+TEACHER_MODEL_PATH = Path("results/samsum/gpt2-xlarge-sft/checkpoint-4256")
+WORK_DIR = Path("./results/samsum/gpt2-base-kd")  # Path('results/samsum/gpt2-base-sft')
+
+tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
+tokenizer.padding_side = "left"
+tokenizer.pad_token_id = tokenizer.eos_token_id
+
+logger.debug(f"The padding token id is {tokenizer.pad_token_id}")
+
+train_data = samsum.get_train(tokenizer)
+val_data = samsum.get_val(tokenizer)
+                        
+model = AutoModelForCausalLM.from_pretrained(MODEL_PATH, 
+                                             torch_dtype=torch.float16, 
+                                             load_in_8bit=False,
+                                             use_cache=False) 
+
+teacher_model = AutoModelForCausalLM.from_pretrained(TEACHER_MODEL_PATH,
+                                                     torch_dtype=torch.float16,
+                                                     load_in_8bit=False,
+                                                     use_cache=False)
+teacher_model.eval()
+
+logger.debug(f"Student model #Params: {sum(p.numel() for p in model.parameters() if p.requires_grad):,}")
+logger.debug(f"Teacher model #Params: {sum(p.numel() for p in teacher_model.parameters() if p.requires_grad):,}")
+
+data_collator = DataCollatorForSeq2Seq(
+    tokenizer,
+    model,
+    label_pad_token_id=tokenizer.pad_token_id,
+    return_tensors="pt",
+    pad_to_multiple_of=8
+)
+
+N_EPOCHS = 20
+LR = 5e-4  # base
+# LR = 1e-7  # Debug
+# LR = 5e-5  # xlarge
+
+PER_DEVICE_BATCH_SIZE = 4
+GRADIENT_ACCUMULATION_STEPS = 4
+
+training_args = Seq2SeqTrainingArguments(
+    output_dir=str(WORK_DIR),
+    per_device_train_batch_size=PER_DEVICE_BATCH_SIZE,
+    per_device_eval_batch_size=PER_DEVICE_BATCH_SIZE,
+    learning_rate=LR,
+    lr_scheduler_type="cosine",
+    fp16=True,
+    warmup_steps=100,
+    weight_decay=1e-2,
+    optim="adamw_torch",
+    adam_epsilon=1e-3,  # thanks to https://zhuanlan.zhihu.com/p/507889212
+    num_train_epochs=N_EPOCHS,
+    gradient_accumulation_steps=GRADIENT_ACCUMULATION_STEPS,
+    save_strategy="steps",
+    evaluation_strategy="steps",
+    eval_steps=0.5/N_EPOCHS,
+    save_steps=0.5/N_EPOCHS,
+    logging_strategy="steps",
+    logging_steps=0.05/N_EPOCHS,
+    logging_dir=str(WORK_DIR/"logs"),
+    save_total_limit=3,
+    load_best_model_at_end=True,
+    report_to="tensorboard",
+    deepspeed="ds_config/ds_config_zero1.json"
+)
+kd_args = Seq2SeqKDArguments()
+
+trainer = Seq2SeqKDTrainer(
+    model=model,
+    teacher_model=teacher_model,
+    tokenizer=tokenizer,
+    args=training_args,
+    kd_args=kd_args,
+    data_collator=data_collator,
+    train_dataset=train_data,
+    eval_dataset=val_data
+)
+logger.info("Start training!!!")
+trainer.train()
---
+++ src/trainer/kd_trainer.py
@@ -0,0 +1,212 @@
+from copy import deepcopy
+from pathlib import Path
+from typing import TYPE_CHECKING, Any, Callable, Dict, List, Optional, Tuple, Union
+from dataclasses import dataclass, field
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset
+
+from transformers import Seq2SeqTrainer, Seq2SeqTrainingArguments
+
+from transformers.trainer import (
+    is_sagemaker_mp_enabled, 
+    is_peft_available, 
+    unwrap_model, 
+    is_apex_available,
+    MODEL_FOR_CAUSAL_LM_MAPPING_NAMES,
+)
+
+# from transformers.utils import logging
+# logger = logging.get_logger(__name__)
+from loguru import logger
+
+if is_apex_available():
+    from apex import amp
+    
+if is_peft_available():
+    from peft import PeftModel
+
+if is_sagemaker_mp_enabled():
+    import smdistributed.modelparallel.torch as smp
+    from smdistributed.modelparallel import __version__ as SMP_VERSION
+    from transformers.trainer import smp_forward_backward
+
+    IS_SAGEMAKER_MP_POST_1_10 = version.parse(SMP_VERSION) >= version.parse("1.10")
+
+    from .trainer_pt_utils import smp_forward_backward, smp_forward_only, smp_gather, smp_nested_concat
+else:
+    IS_SAGEMAKER_MP_POST_1_10 = False
+
+if TYPE_CHECKING:
+    from transformers.data.data_collator import DataCollator
+    from transformers.modeling_utils import PreTrainedModel
+    from transformers.tokenization_utils_base import PreTrainedTokenizerBase
+    from transformers.trainer_callback import TrainerCallback
+    from transformers.trainer_utils import EvalPrediction, PredictionOutput
+    from transformers.training_args import TrainingArguments
+
+@dataclass
+class Seq2SeqKDArguments:
+    reverse_kld: bool = field(default=False, metadata={"help": "Whether to use reverse KL divergence."}) 
+    kd_ratio: float = field(default=0.5, metadata={"help": "Weight for KD loss."}) 
+
+
+class Seq2SeqKDTrainer(Seq2SeqTrainer):
+    def __init__(
+        self,
+        model: Union["PreTrainedModel", nn.Module] = None,
+        teacher_model: Union["PreTrainedModel", nn.Module] = None,
+        args: "TrainingArguments" = None,
+        kd_args: "Seq2SeqKDArguments" = None,
+        data_collator: Optional["DataCollator"] = None,
+        train_dataset: Optional[Dataset] = None,
+        eval_dataset: Optional[Union[Dataset, Dict[str, Dataset]]] = None,
+        tokenizer: Optional["PreTrainedTokenizerBase"] = None,
+        model_init: Optional[Callable[[], "PreTrainedModel"]] = None,
+        compute_metrics: Optional[Callable[["EvalPrediction"], Dict]] = None,
+        callbacks: Optional[List["TrainerCallback"]] = None,
+        optimizers: Tuple[torch.optim.Optimizer, torch.optim.lr_scheduler.LambdaLR] = (None, None),
+        preprocess_logits_for_metrics: Optional[Callable[[torch.Tensor, torch.Tensor], torch.Tensor]] = None,
+    ):
+        super().__init__(
+            model=model,
+            args=args,
+            data_collator=data_collator,
+            train_dataset=train_dataset,
+            eval_dataset=eval_dataset,
+            tokenizer=tokenizer,
+            model_init=model_init,
+            compute_metrics=compute_metrics,
+            callbacks=callbacks,
+            optimizers=optimizers,
+            preprocess_logits_for_metrics=preprocess_logits_for_metrics,
+        )
+        
+        # Load Teacher model
+        self.teacher_model = teacher_model
+        self._move_model_to_device(self.teacher_model, args.device)
+        logger.debug(f"Teacher model is moved to device {args.device}")
+        self.kd_args = kd_args
+        self.loss_dict = dict()
+        
+    def kd_loss(self, input, target, data):
+        loss_mask = torch.where(((data['labels'] < 0) | (data['labels']==self.tokenizer.pad_token_id)), 0, 1).unsqueeze(-1)
+        def kld_loss(input, target, mask):
+            n_data = len(input)
+            # Not train on input
+            # unsqueeze for the convenience of later computations
+            kld = F.kl_div((input*mask).nan_to_num(0,0,0), (target*mask).nan_to_num(0,0,0), reduction="none")
+            # lambda input, target, mask: (F.kl_div(input.log()*mask, target*mask, reduction="none") / mask.sum(1, keepdim=True)).view(len(input),-1).sum(-1).mean()
+            return (kld/mask.sum(1, keepdim=True)).view(n_data,-1).nansum(-1).mean()
+        
+        if self.kd_args.reverse_kld:
+            input, target = target, input
+            
+        input = F.log_softmax(input, dim=-1)  # , dtype=torch.float32)
+        target = F.softmax(target, dim=-1)  # , dtype=torch.float32)
+
+        # __import__('ipdb').set_trace()
+        return kld_loss(input, target, loss_mask)
+       
+    def training_step(self, model: nn.Module, inputs: Dict[str, Union[torch.Tensor, Any]]) -> torch.Tensor:
+        """
+        Perform a training step on a batch of inputs.
+
+        Subclass and override to inject custom behavior.
+
+        Args:
+            model (`nn.Module`):
+                The model to train.
+            inputs (`Dict[str, Union[torch.Tensor, Any]]`):
+                The inputs and targets of the model.
+
+                The dictionary will be unpacked before being fed to the model. Most models expect the targets under the
+                argument `labels`. Check your model's documentation for all accepted arguments.
+
+        Return:
+            `torch.Tensor`: The tensor with training loss on this batch.
+        """
+        model.train()
+        inputs = self._prepare_inputs(inputs)
+
+        if is_sagemaker_mp_enabled():
+            loss_mb = smp_forward_backward(model, inputs, self.args.gradient_accumulation_steps)
+            return loss_mb.reduce_mean().detach().to(self.args.device)
+
+        with self.compute_loss_context_manager():
+            loss = self.compute_loss(model, inputs)
+
+        if self.args.n_gpu > 1:
+            loss = loss.mean()  # mean() to average on multi-gpu parallel training
+
+        if self.do_grad_scaling:
+            self.scaler.scale(loss).backward()
+        elif self.use_apex:
+            with amp.scale_loss(loss, self.optimizer) as scaled_loss:
+                scaled_loss.backward()
+        else:
+            self.accelerator.backward(loss)
+
+        return loss.detach() / self.args.gradient_accumulation_steps
+    
+    def compute_loss(self, model, inputs, return_outputs=False):
+        """
+        How the loss is computed by Trainer. By default, all models return the loss in the first element.
+
+        Subclass and override for custom behavior.
+        """
+        if self.label_smoother is not None and "labels" in inputs:
+            labels = inputs.pop("labels")
+        else:
+            labels = None  # Default case: no label smoothing
+        outputs = model(**inputs)
+        
+        # Save past state if it exists
+        # TODO: this needs to be fixed and made cleaner later.
+        if self.args.past_index >= 0:
+            self._past = outputs[self.args.past_index]
+
+        if labels is not None:
+            if is_peft_available() and isinstance(model, PeftModel):
+                model_name = unwrap_model(model.base_model)._get_name()
+            else:
+                model_name = unwrap_model(model)._get_name()
+            if model_name in MODEL_FOR_CAUSAL_LM_MAPPING_NAMES.values():
+                loss = self.label_smoother(outputs, labels, shift_labels=True)
+            else:
+                loss = self.label_smoother(outputs, labels)
+        else:
+            if isinstance(outputs, dict) and "loss" not in outputs:
+                raise ValueError(
+                    "The model did not return a loss from the inputs, only the following keys: "
+                    f"{','.join(outputs.keys())}. For reference, the inputs it received are {','.join(inputs.keys())}."
+                )
+            # We don't use .loss here since the model may return tuples instead of ModelOutput.
+            loss_sft = outputs["loss"] if isinstance(outputs, dict) else outputs[0]
+
+        # __import__('ipdb').set_trace()
+        loss_kd = self.compute_kd_loss(model, inputs, outputs)
+        kd_ratio = self.kd_args.kd_ratio
+        loss = (1-kd_ratio)* loss_sft + kd_ratio * loss_kd
+        self.loss_dict = dict(
+            loss=loss.mean().item(),
+            loss_sft=loss_sft.mean().item(),
+            loss_kd=loss_kd.mean().item()
+        )
+        self.log(self.loss_dict)
+        return (loss, outputs) if return_outputs else loss
+    
+    def compute_kd_loss(self, model, inputs, outputs=None):
+        assert outputs, "Give me student model's outputs."
+        # KD !!!
+        with torch.no_grad():
+            logits_t = self.teacher_model(**inputs)["logits"]
+        loss_kd = self.kd_loss(outputs["logits"], logits_t, inputs)
+        # logger.debug(f"loss_kd: {loss_kd.item():.4f}")
+        return loss_kd
+    
+class LDKD(Seq2SeqKDTrainer):
+    def compute_loss(self, model, inputs, return_outputs=False):
+        pass
